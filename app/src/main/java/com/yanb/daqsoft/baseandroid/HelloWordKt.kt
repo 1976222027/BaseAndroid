2e78b0713c473fd9663699051155126cec0adcf8
Tomsyb <760375443@qq.com>
2019-05-10 00:21:30 +0800

    网络请求搭建

---
+++ app/src/main/java/com/yanb/daqsoft/baseandroid/HelloWordKt.kt
@@ -0,0 +1,15 @@
+package com.yanb.daqsoft.baseandroid
+
+import android.util.Log
+
+//boolean类型
+val mBoolead: Boolean = true
+
+val mStrName: String = "firstStr"
+val mStrAge: String = "13"
+fun main(args: Array<String>) {
+    Log.e("df","sdfs")
+    if (mStrName== mStrAge){
+
+    }
+}
