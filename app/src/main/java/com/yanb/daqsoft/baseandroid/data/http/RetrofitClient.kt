5f1c4a189a70c6c17d99c0cf198cc46ba9ba2afd
yanbo <760375443@qq.com>
2019-08-09 20:19:04 +0800

    mvvm框架学习

--- app/src/main/java/com/yanb/daqsoft/baseandroid/data/http/RetrofitClient.kt
+++ app/src/main/java/com/yanb/daqsoft/baseandroid/data/http/RetrofitClient.kt
@@ -1,5 +1,9 @@
 package com.yanb.daqsoft.baseandroid.data.http
 
 class RetrofitClient {
-
+    // 超时时间
+    val DEFAULT_TIMEOUT:Int = 20
+    // 缓存时间
+    val CACHE_TIMEOUT:Int = 10*1024*1024
+    var URL_BASE = "https://www.oschina.net/"
 }
