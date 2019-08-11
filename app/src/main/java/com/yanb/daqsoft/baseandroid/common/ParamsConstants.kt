1485646e5b769bfed822357ce97778fda4b31e3c
Tomsyb <760375443@qq.com>
2019-08-11 22:57:24 +0800

    登录界面搭建

---
+++ app/src/main/java/com/yanb/daqsoft/baseandroid/common/ParamsConstants.kt
@@ -0,0 +1,10 @@
+package com.yanb.daqsoft.baseandroid.common
+
+/**
+ * 参数常量
+ */
+object ParamsConstants {
+
+    const val PARAMS_SITECODE_VALUE = "nngjapp"
+    const val PARAMS_SITECODE_KTY = "siteCode"
+}
---
+++ app/src/main/java/com/yanb/daqsoft/baseandroid/login/entity/User.kt
@@ -0,0 +1,6 @@
+package com.yanb.daqsoft.baseandroid.login.entity
+
+/**
+ * 登录用户实体类
+ */
+data class User(val head:String,val name:String,val account:String)
